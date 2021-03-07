277288a4e239b800188a176fff51da25c0ffb148
Jared Long <jlong4223@gmail.com>
2021-03-07 11:26:56 -0600

    set up routes and controllers for each

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+// module
 let weatherApp = angular.module("weatherApp", ["ngRoute", "ngResource"]);
+
+// routes
+weatherApp.config(function ($routeProvider) {
+  $routeProvider
+
+    .when("/", {
+      templateUrl: "pages/home.htm",
+      controller: "homeController",
+    })
+
+    .when("/forecast", {
+      templateUrl: "pages/forecast.htm",
+      controller: "forecastController",
+    });
+});
+
+// controllers
+weatherApp.controller("homeController", ["$scope", function ($scope) {}]);
+weatherApp.controller("forecastController", ["$scope", function ($scope) {}]);
