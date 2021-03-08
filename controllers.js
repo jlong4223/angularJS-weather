af2b049cc0d6828fe5111ff792f8f60a80c5001e
Jared Long <jlong4223@gmail.com>
2021-03-08 12:21:20 -0600

    injects the weatherService and removes api fetch from here  and passes  info

--- controllers.js
+++ controllers.js
@@ -19,25 +19,33 @@ weatherApp.controller("forecastController", [
   "cityService",
   "$resource",
   "$routeParams",
-  function ($scope, cityService, $resource, $routeParams) {
+  "weatherService",
+  function ($scope, cityService, $resource, $routeParams, weatherService) {
     $scope.city = cityService.city;
 
     // TODO have a ng-model that allows user to select day as well
     $scope.days = $routeParams.days || "2";
 
-    // defining the API url as the resource
-    $scope.weatherAPI = $resource(
-      "http://api.openweathermap.org/data/2.5/forecast",
-      { callback: "JSON_CALLBACK" },
-      { get: { method: "JSONP" } }
-    );
+    $scope.key = "5b3c5a41e420b342a7d2e498f5e3fd82";
+    // // defining the API url as the resource
+    // $scope.weatherAPI = $resource(
+    //   "http://api.openweathermap.org/data/2.5/forecast",
+    //   { callback: "JSON_CALLBACK" },
+    //   { get: { method: "JSONP" } }
+    // );
 
     // grabbing the data from the resource with additional parameters
-    $scope.weatherResult = $scope.weatherAPI.get({
-      q: $scope.city,
-      cnt: $scope.days,
-      appid: "5b3c5a41e420b342a7d2e498f5e3fd82",
-    });
+    // $scope.weatherResult = $scope.weatherAPI.get({
+    //   q: $scope.city,
+    //   cnt: $scope.days,
+    //   appid: "5b3c5a41e420b342a7d2e498f5e3fd82",
+    // });
+
+    $scope.weatherResult = weatherService.GetWeather(
+      $scope.city,
+      $scope.days,
+      $scope.key
+    );
 
     $scope.convertToF = function (degK) {
       return Math.round((degK - 273) * 1.8 + 32);
