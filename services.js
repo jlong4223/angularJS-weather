1518aab8bc5d61d3779278c8d1023436436aa51f
Jared Long <jlong4223@gmail.com>
2021-03-08 12:20:31 -0600

    moves weather api call to services

--- services.js
+++ services.js
@@ -2,3 +2,24 @@
 weatherApp.service("cityService", function () {
   this.city = "Austin, Texas";
 });
+
+weatherApp.service("weatherService", [
+  "$resource",
+  function ($resource) {
+    this.GetWeather = function (city, days, key) {
+      // defining the API url as the resource
+      let weatherAPI = $resource(
+        "http://api.openweathermap.org/data/2.5/forecast",
+        { callback: "JSON_CALLBACK" },
+        { get: { method: "JSONP" } }
+      );
+
+      // grabbing the data from the resource with additional parameters
+      return weatherAPI.get({
+        q: city,
+        cnt: days,
+        appid: key,
+      });
+    };
+  },
+]);
